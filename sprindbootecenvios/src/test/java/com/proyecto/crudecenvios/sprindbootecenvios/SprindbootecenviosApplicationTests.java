0f8ca54f8c3f02c1b5221c54a984d407f658c3ff
rootEl <11406509+BolivarCaro@users.noreply.github.com>
2024-07-07 10:59:35 -0500

    creacion de la pagina web en springboot

---
+++ sprindbootecenvios/src/test/java/com/proyecto/crudecenvios/sprindbootecenvios/SprindbootecenviosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.crudecenvios.sprindbootecenvios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprindbootecenviosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
